c774ca5489fb243afef55c5925aff94e18c4d9e2
kjc-PC\kjc <whd4028@gmail.com>
2017-07-04 00:48:00 +0900

    DB 파일 추가 index.jsp 이미지 center 정렬

---
+++ ex00/src/main/java/org/zerock/web/ProductVO.java
@@ -0,0 +1,38 @@
+package org.zerock.web;
+
+public class ProductVO {
+
+	String name;
+	int price;
+	
+	public ProductVO(String name, int price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "ProductVO [name=" + name + ", price=" + price + "]";
+	}
+	
+	
+	
+	
+}
